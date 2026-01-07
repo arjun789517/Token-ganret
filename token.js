07ffba8d45554970d4cde3b5d0da425d3bd0d20f
arjun789517 <arjuntahkur20@gmail.com>
2026-01-08 01:51:16 +0530

    Create token.js

---
+++ token.js
@@ -0,0 +1,36 @@
+const qs = require('querystring');
+const axios = require('axios-proxy-fix');
+const uuid = require('uuid/v4');
+const utils = require('./utils');
+
+async function getToken(email, password) {
+  const sim = utils.randBetween(2e4, 4e4);
+  let deviceID = uuid();
+  let adID = uuid();
+  let formData = {
+    adid: adID,
+    format: 'json',
+    device_id: deviceID,
+    email: email,
+    password: password,
+    cpl: 'true',
+    family_device_id: deviceID,
+    credentials_type: 'device_based_login_password',
+    generate_session_cookies: '1',
+    error_detail_type: 'button_with_disabled',
+    source: 'device_based_login',
+    machine_id: utils.randString(24),
+    meta_inf_fbmeta: '',
+    advertiser_id: adID,
+    currently_logged_in_userid: '0',
+    locale: 'en_US',
+    client_country_code: 'US',
+    method: 'auth.login',
+    fb_api_req_friendly_name: 'authenticate',
+    fb_api_caller_class: 'com.facebook.account.login.protocol.Fb4aAuthHandler',
+    api_key: '882a8490361da98702bf97a021ddc14d'
+  };
+  formData.sig = getSig(utils.sortObj(formData));
+  let conf = {
+    url: 'https://b-api.facebook.com/method/auth.login',
+    method: 'post',
